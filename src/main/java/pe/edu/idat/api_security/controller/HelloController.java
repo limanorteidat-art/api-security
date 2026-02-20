de1ebd8d470ce0ae8d36ec622b3c004842c1d9f2
limanorteidat-art <limanorteidat@gmail.com>
2026-02-20 09:49:08 -0500

    ejercicio terminado

--- src/main/java/pe/edu/idat/api_security/controller/HelloController.java
+++ src/main/java/pe/edu/idat/api_security/controller/HelloController.java
@@ -1,4 +1,20 @@
 package pe.edu.idat.api_security.controller;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@PreAuthorize("hasRole('ADMIN')")
+@RestController
+@RequestMapping("/api/v1/hello")
 public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<String> getWelcome(){
+        return ResponseEntity.ok(
+                "Bienvenido al proyecto backend");
+    }
 }
